7044589b72cde31cd8d1962a4862806d7ccd1f78
Loizeau <camille.loizeau@supdevinci-edu.fr>
2025-02-11 14:53:20 +0100

    feat/visuel appli + formulaire auth

--- app.py
+++ app.py
@@ -1,4 +1,48 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("Bienvenue dans votre première application Streamlit.")
+st.set_page_config(page_title="Content Moderator Pro", page_icon=":camera:", layout="centered", initial_sidebar_state="auto", menu_items=None)
+
+st.title(":camera_with_flash: Content Moderator Pro")
+st.header(":mag: Analysez et modérez votre contenu en un clic !")
+st.caption(":warning: Veuillez configurer vos credentials AWS dans la barre latérale")
+
+if "show_keys" not in st.session_state:
+    st.session_state.show_keys = False
+
+
+def toggle_visibility():
+    st.session_state.show_keys = not st.session_state.show_keys
+
+with st.sidebar:
+    st.title(":gear: Configuration")
+    st.write(":key: Credentials AWS")
+    st.button(":open_file_folder: Charger credentials depuis .env")
+    # Utilisation de HTML/CSS pour styliser un bouton en vert
+    button_style = """
+        <style>
+        div.stButton > button {
+            background-color: green !important;
+            color: white !important;
+            padding: 10px 20px;
+            font-size: 16px;
+            transition: background-color 0.3s ease;
+    }
+    div.stButton > button:hover {
+        background-color: darkgreen !important;
+        color: white !important;
+    }
+        }
+        </style>
+    """
+
+    st.markdown(button_style, unsafe_allow_html=True)
+
+    with st.form("login_form"):
+        access_key = st.text_input("Access Key", type="password" if not st.session_state.show_keys else "default")
+        secret_key = st.text_input("Secret Key", type="password" if not st.session_state.show_keys else "default")
+        bucket_name = st.text_input("Nom du bucket S3")
+
+
+
+        # Bouton de validation
+        st.form_submit_button("Se Connecter")
--- moderation.py
+++ moderation.py
@@ -1,5 +1,20 @@
-import os
-import cv2
+import os, cv2, boto3
+from dotenv import load_dotenv
+
+#Fonction de création de session AWS
+def get_aws_session():
+    # Charge les variables d'environnement depuis .env.
+    load_dotenv()
+
+    # Crée une session AWS avec les clés d'accès et la région définies dans les variables d'environnement.
+    aws_session = boto3.Session(
+        aws_access_key_id=os.getenv("ACCESS_KEY"),        # Récupère l'ID de clé d'accès depuis les variables d'environnement.
+        aws_secret_access_key=os.getenv("SECRET_KEY"),    # Récupère la clé d'accès secrète depuis les variables d'environnement.
+        region_name="us-east-1"                           # Spécifie la région AWS à utiliser.
+    )
+    
+    # Retourne l'objet session créé.
+    return aws_session
 
 #Fonction d'analyse du type du fichier (image ou vidéo)
 def check_filetype(filename):   
@@ -37,3 +52,17 @@ def extract_frame_video(video_path, frame_id):
     # Si la lecture réussit (ret est True), retourne l'image.
     # Sinon, retourne None.
     return image if ret else None
+
+#Fonction pour modérer une image (reconnaitre les pb)
+def moderate_image(image_path, aws_service):
+    # Lire l'image en mode binaire
+    with open(image_path, "rb") as image_file:
+        image_bytes = image_file.read()
+
+    # Appel au service AWS Rekognition
+    response = aws_service.detect_moderation_labels(Image={"Bytes": image_bytes})
+
+    # Extraire les étiquettes de modération
+    labels = [label["Name"] for label in response.get("ModerationLabels", [])]
+
+    return labels
